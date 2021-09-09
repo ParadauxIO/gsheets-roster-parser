ddd2224a8df377392eb7183b07367a8518398f71
Rían Errity <rian@paradaux.io>
2021-09-09 01:06:35 +0100

    Python naming standards

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ def main():
     roster = roster_parser.roster_to_json("roster.txt")
     date = roster["date"]
 
-    eventCount = 0
+    event_count = 0
 
     for day in roster["times"]:
         for time in day:
-            roster_calendar_api.createEvent(date, time, roster["titles"][eventCount])
-            eventCount += 1
+            roster_calendar_api.createEvent(date, time, roster["titles"][event_count])
+            event_count += 1
         date += timedelta(days=1)
 
-    print(f"Created {eventCount} events in your calendar.")
+    print(f"Created {event_count} events in your calendar.")
 
 
 if __name__ == '__main__':
